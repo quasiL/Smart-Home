e320df619fb0d86a388d6d99add809c76122e3f8
Artur Khairullin <khairart@fel.cvut.cz>
2022-12-26 20:31:14 +0100

    TASK_04 delete unused imports in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,3 @@
-import model.device.LightController;
-import model.house.SimpleHouse;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.concurrent.TimeUnit;
-
 public class Main
 {
     public static void main(String[] args)
